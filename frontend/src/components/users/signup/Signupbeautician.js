7ec846f1a3b340b64391ec8ee319f1d3b0275239
sooraj667 <soorajbaijucet22@gmail.com>
2023-10-25 09:55:57 +0530

    commit 171

--- frontend/src/components/users/signup/Signupbeautician.js
+++ frontend/src/components/users/signup/Signupbeautician.js
@@ -1,67 +1,67 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Signupform from "./Signupform";
 import { useSelector, useDispatch } from "react-redux";
 import axiosInstance from "../../../axios/axiosconfig";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
-import { Grid } from "@mui/material";
+import { Avatar, Grid } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { setBeautotp } from "../../../feautures/otpslice";
-import {submitForm} from "../../../feautures/beautslice"
+import { submitForm } from "../../../feautures/beautslice";
 import MuiAlert from "@mui/material/Alert";
-import toast, { Toaster } from 'react-hot-toast';
-
-
+import toast, { Toaster } from "react-hot-toast";
+import signuppic from "../../../images/Barber-rafiki (1).png";
 
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-
-
 const Signupbeautician = () => {
   const formdatas = useSelector((state) => state.signup);
-  const [alreadyTaken,setAlreadyTaken]=useState(null)
+  const [alreadyTaken, setAlreadyTaken] = useState(null);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const handleSubmit = () => {
-    dispatch(submitForm())
+    dispatch(submitForm());
+    if (formdatas.value.error.submiterror) {
+      toast.error("ERROR");
+    }
+
     const datas = {
       pname: formdatas.value.pname,
       email: formdatas.value.email,
       phone: formdatas.value.phone,
       password: formdatas.value.password,
     };
-    if (formdatas.value.pname==="" || formdatas.value.email==="" || formdatas.value.phone==="" || formdatas.value.password===""){
-      
-      return
+    if (
+      formdatas.value.pname === "" ||
+      formdatas.value.email === "" ||
+      formdatas.value.phone === "" ||
+      formdatas.value.password === ""
+    ) {
+      return;
     }
     axiosInstance
       .post("beaut/signup/", datas)
       .then((response) => {
-        if(response.data.message==="Email-Failed"){
-          setAlreadyTaken("Email already taken!")
-          return
-
+        if (response.data.message === "Email-Failed") {
+          setAlreadyTaken("Email already taken!");
+          return;
         }
 
-        if(response.data.message==="Phone-Failed"){
-          setAlreadyTaken("Phone number already taken!")
-          return
-
+        if (response.data.message === "Phone-Failed") {
+          setAlreadyTaken("Phone number already taken!");
+          return;
         }
-
 
         console.log("SUCCESSFULL");
         console.log(response.data);
         dispatch(setBeautotp());
-        toast.success('Otp Sent to your mail! Check')
-        setTimeout(()=>{
-                    
-        navigate("../otp/")
-
-          },3000)
+        toast.success("Otp Sent to your mail! Check");
+        setTimeout(() => {
+          navigate("../otp/");
+        }, 3000);
 
         // navigate("../otp/");
       })
@@ -71,181 +71,141 @@ const Signupbeautician = () => {
   };
 
   return (
-    <div className="row mainrow">
-      <Toaster/>
-      <div className="col-3">
-
+    <div className="mainrow">
+      <Toaster />
+      <div className="sub-heading-div flex justify-center align-center py-3 text-small fw-2 sgfont  themecolor ">
+        SIGNUP BEAUTICIAN
       </div>
-      <div className="col-6">
-      <Paper
-          elevation={24}
-          sx={{
-            // width: 600,
-            // height: 510,
-            padding:3,
-            backgroundColor: "whitesmoke",
-            // backgroundImage:'url("https://img.freepik.com/premium-photo/close-up-hair-supplies-flat-lay_23-2148352942.jpg?w=900")',
-            objectFit: "cover",
-            backgroundRepeat: "no-repeat",
-            // marginLeft:"25vw",
-            // marginRight:"25vw",
-            marginTop: "20px",
-            marginBottom:10,
-            opacity: [0.9, 0.8, 0.8],
+      <div className="flex justify-around align-center">
+        <div className="first">
+          <Avatar src={signuppic} sx={{ width: 410, height: 410 }}></Avatar>
+        </div>
+        <div className="first">
+          <Signupform />
+          {formdatas.value.errorcheck == false && (
+            <Button
+              
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 ,ml:3}}
+              onClick={handleSubmit}
+             
+            >
+              Sign Up
+            </Button>
+          )}
+          <div className="mb-5 ml-5">
+            {formdatas.value.error.submiterror && (
+              <Alert severity="error" sx={{}}>
+                {formdatas.value.error.submiterror}
+              </Alert>
+            )}
 
-            "&:hover": {
-              backgroundColor: "whitesmoke",
-              opacity: [0.9, 0.8, 0.7],
-            },
-          }}
-          className="paper"
-        >
-          <div className=""></div>
-          <div className="row">
-            <div class="container mt-5">
-              <div class="row justify-content-center">
-                <div class="col-md-6">
-                  <div class="">
-                    <div>
-                      <h3 class="text-center">Beautician Sign Up</h3>
-                    </div>
-                    <Signupform />
-                    {formdatas.value.errorcheck == false && (
-                      <Button
-                        variant="contained"
-                        onClick={handleSubmit}
-                        sx={{
-                          marginLeft: "100px",
-                          marginBottom:8,
-                        }}
-                      >
-                        Sign Up
-                      </Button>
-                    )}
+            {alreadyTaken && (
+              <Alert severity="error" sx={{}}>
+                {alreadyTaken}
+              </Alert>
+            )}
 
-                  
-                  </div>
-
-
-                  {formdatas.value.error.submiterror&&
-                  <Alert
-                  severity="error"
-                  sx={{
-                    
-                   
-                  }}
-                >
-                  {formdatas.value.error.submiterror}
-                </Alert>
-
-
-                  }
-
-
-                  {alreadyTaken &&
-                  <Alert
-                  severity="error"
-                  sx={{
-                    
-                  }}
-                >
-                  {alreadyTaken}
-                </Alert>
-
-                  }
-
-                  
-
-                      
-                  {/* <div className="text-danger ml-5 pb-5 ">{formdatas.value.error.submiterror}</div>
+            {/* <div className="text-danger ml-5 pb-5 ">{formdatas.value.error.submiterror}</div>
                   <div className="text-danger ml-5 pb-5">{alreadyTaken}</div> */}
-                </div>
-              </div>
-            </div>
           </div>
-        </Paper>
-        
-
+        </div>
       </div>
-      <div className="col-3 ">
-
-      </div>
-    
     </div>
+    // <div className="row mainrow">
+    //   <Toaster />
 
+    //   <div className="flex justify-center">
+    //     <div className="col-6 flex justify-center align-center">
+    //     <Avatar src={signuppic} sx={{width:410,height:410}}></Avatar>
 
+    //     </div>
+    //     <div className="col-6 justify-center">
+    //     <Paper
+    //       elevation={24}
+    //       sx={{
+    //           // width: 600,
+    //           // height: 510,
+    //         paddingInline: 5,
+    //         backgroundColor: "whitesmoke",
+    //         // backgroundImage:'url("https://img.freepik.com/premium-photo/close-up-hair-supplies-flat-lay_23-2148352942.jpg?w=900")',
+    //         objectFit: "cover",
+    //         backgroundRepeat: "no-repeat",
+    //         // marginLeft:"25vw",
+    //         // marginRight:"25vw",
+    //         marginTop: "20px",
+    //         marginBottom: 10,
+    //         opacity: [0.9, 0.8, 0.8],
 
+    //         "&:hover": {
+    //           backgroundColor: "whitesmoke",
+    //           opacity: [0.9, 0.8, 0.7],
+    //         },
+    //       }}
+    //       className=""
+    //     >
+    //       <div className=""></div>
+    //       <div className="row">
+    //         <div class="container mt-5">
+    //           <div class="row justify-content-center">
 
+    //               <div class="">
+    //                 <div>
 
+    //                   <div className="sub-heading-div flex justify-center align-center py-3 text-small fw-2 sgfont  themecolor ">
+    //                     SIGNUP FOR
+    //                   </div>
+    //                   <div className="sub-heading-div flex justify-center align-center py-3 text-medium fw-2 sgfont  themecolor underline">
+    //                     BEAUTICIAN{" "}
 
+    //                   </div>
 
+    //                 </div>
+    //                 <Signupform />
+    //                 {formdatas.value.errorcheck == false && (
+    //                   <Button
+    //                     variant="contained"
+    //                     onClick={handleSubmit}
+    //                     sx={{
+    //                       marginLeft: "100px",
+    //                       marginBottom: 8,
+    //                     }}
+    //                   >
+    //                     Sign Up
+    //                   </Button>
+    //                 )}
+    //               </div>
 
+    // {formdatas.value.error.submiterror && (
+    //   <Alert severity="error" sx={{}}>
+    //     {formdatas.value.error.submiterror}
+    //   </Alert>
+    // )}
 
-  //   <div className="row">
-  //     <div className="col-md-3"></div>
+    // {alreadyTaken && (
+    //   <Alert severity="error" sx={{}}>
+    //     {alreadyTaken}
+    //   </Alert>
+    // )}
 
-  //     {/* <Grid container justifyContent="center">
-  //   <Grid item xs={12} sm={8} md={6}> */}
-  //     <div className="col-md-6  ">
-  //       <Paper
-  //         elevation={24}
-  //         sx={{
-  //           // width: 600,
-  //           // height: 510,
-  //           backgroundColor: "whitesmoke",
-  //           // backgroundImage:'url("https://img.freepik.com/premium-photo/close-up-hair-supplies-flat-lay_23-2148352942.jpg?w=900")',
-  //           objectFit: "cover",
-  //           backgroundRepeat: "no-repeat",
-  //           // marginLeft:"25vw",
-  //           // marginRight:"25vw",
-  //           marginTop: "20px",
-  //           marginBottom:10,
-  //           opacity: [0.9, 0.8, 0.8],
+    // {/* <div className="text-danger ml-5 pb-5 ">{formdatas.value.error.submiterror}</div>
+    // <div className="text-danger ml-5 pb-5">{alreadyTaken}</div> */}
 
-  //           "&:hover": {
-  //             backgroundColor: "whitesmoke",
-  //             opacity: [0.9, 0.8, 0.7],
-  //           },
-  //         }}
-  //         className="paper"
-  //       >
-  //         <div className=""></div>
-  //         <div className="row">
-  //           <div class="container mt-5">
-  //             <div class="row justify-content-center">
-  //               <div class="col-md-6">
-  //                 <div class="">
-  //                   <div>
-  //                     <h3 class="text-center">Beautician Sign Up</h3>
-  //                   </div>
-  //                   <Signupform />
-  //                   {formdatas.value.errorcheck == false && (
-  //                     <Button
-  //                       variant="contained"
-  //                       onClick={handleSubmit}
-  //                       sx={{
-  //                         marginLeft: "100px",
-  //                         marginBottom:8,
-  //                       }}
-  //                     >
-  //                       Sign Up
-  //                     </Button>
-  //                   )}
-  //                 </div>
-  //               </div>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </Paper>
-  //     </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </Paper>
 
-  //     <div className="col-md-3">
-        
-  //       </div>
+    //     </div>
 
-  //     {/* </Grid>
-  // </Grid> */}
-  //   </div>
+    //   </div>
+    //   {/* <div className="col-3"></div> */}
+    //   {/* <div className="col-6">
+
+    //   </div>
+    //   <div className="col-3 "></div> */}
+    // </div>
   );
 };
 
--- frontend/src/components/users/signup/Signupform.js
+++ frontend/src/components/users/signup/Signupform.js
@@ -1,4 +1,16 @@
 import React from "react";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import {
   changePName,
@@ -11,100 +23,271 @@ import {
 import { useDispatch,useSelector } from "react-redux";
 import TextField from "@mui/material/TextField";
 
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const defaultTheme = createTheme();
+
 const Signupform = () => {
   const dispatch = useDispatch();
   const datas=useSelector((state)=>state.signup)
 
   return (
-    <div>
-      <div class="card-body">
-        <div class="form-group">
-          <div className="row">
-            <label for="username" className="mr-3">
-              Name:
-            </label>
-            <TextField
-              id="standard-basic"
-              label=""
-              variant="standard"
-              // type="text"
-              // id="username"
-              // name="username"
-              // class="form-control"
-              // required
-              onChange={(e) => dispatch(changePName(e.target.value))}
-            />
-          </div>
-          <span className="text-danger">{datas.value.error.pname}</span>
-        </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={""} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="Name"
+                  label="Name"
+                  autoFocus
+                  onChange={(e) => dispatch(changePName(e.target.value))}
+                />
+                <span className="text-danger">{datas.value.error.pname}</span>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Email"
+                  name="lastName"
+                  autoComplete="family-name"
+                  onChange={(e) => dispatch(changeEmail(e.target.value))}
+                />
+                <span className="text-danger">{datas.value.error.email}</span>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Phone"
+                  name="email"
+                  autoComplete="email"
+                  onChange={(e) => dispatch(changePhone(e.target.value))}
+                />
+                <span className="text-danger">{datas.value.error.phone}</span>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={(e) => dispatch(changePassword(e.target.value))}
+                />
+              </Grid>
 
-        <div class="form-group">
-          <div className="row">
-            <label for="email" className="mr-3">
-              Email:
-            </label>
-            <TextField
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Re-enter Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={(e) => dispatch(changeCpassword(e.target.value))}
+                />
+                <span className="text-danger">{datas.value.error.cpassword}</span>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            {/* <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button> */}
+            {/* <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid> */}
+          </Box>
+        </Box>
+        {/* <Copyright sx={{ mt: 5 }} /> */}
+      </Container>
+    </ThemeProvider>
+
+
+
+
+
+
+    // <div>
+    //   <div className="flex jsutify-around">
+    //     <div className="f">
+    //     <label for="username" className="mr-3">
+    //           Name:
+    //         </label>
+    //         <input
+    //           id="standard-basic"
+    //           label=""
+    //           className="form-control"
+    //           // type="text"
+    //           // id="username"
+    //           // name="username"
+    //           // class="form-control"
+    //           // required
+    //           onChange={(e) => dispatch(changePName(e.target.value))}
+    //         />
+
+    //     </div>
+    //     <div className="s">
+    //     <label for="username" className="mr-3">
+    //           Name:
+    //         </label>
+    //         <input
+    //           id="standard-basic"
+    //           label=""
+    //           className="form-control"
+    //           // type="text"
+    //           // id="username"
+    //           // name="username"
+    //           // class="form-control"
+    //           // required
+    //           onChange={(e) => dispatch(changePName(e.target.value))}
+    //         />
+
+    //     </div>
+
+
+    //   </div>
+    //   <div class="card-body">
+    //     <div class="form-group">
+    //       <div className="row">
+    //         <label for="username" className="mr-3">
+    //           Name:
+    //         </label>
+    //         <input
+    //           id="standard-basic"
+    //           label=""
+    //           className="form-control"
+    //           // type="text"
+    //           // id="username"
+    //           // name="username"
+    //           // class="form-control"
+    //           // required
+    //           onChange={(e) => dispatch(changePName(e.target.value))}
+    //         />
+            
+    //       </div>
+    //       <span className="text-danger">{datas.value.error.pname}</span>
+    //     </div>
+
+    //     <div class="form-group">
+    //       <div className="row">
+    //         <label for="email" className="mr-3">
+    //           Email:
+    //         </label>
+    //         <input
               
-              variant="standard"
-              onChange={(e) => dispatch(changeEmail(e.target.value))}
-            />
-          </div>
-          <span className="text-danger">{datas.value.error.email}</span>
-        </div>
+    //           className="form-control"
+    //           onChange={(e) => dispatch(changeEmail(e.target.value))}
+    //         />
+    //       </div>
+    //       <span className="text-danger">{datas.value.error.email}</span>
+    //     </div>
 
-        <div class="form-group">
-          <div className="row">
-            <label for="password" className="mr-3">
-              Phone:
-            </label>
-            <TextField
-              type="numtextber"
+    //     <div class="form-group">
+    //       <div className="row">
+    //         <label for="password" className="mr-3">
+    //           Phone:
+    //         </label>
+    //         <input
+    //           type="numtextber"
             
-              variant="standard"  
+    //           className="form-control" 
             
-              required
-              onChange={(e) => dispatch(changePhone(e.target.value))}
-            />
-          </div>
-          <span className="text-danger">{datas.value.error.phone}</span>
-        </div>
+    //           required
+    //           onChange={(e) => dispatch(changePhone(e.target.value))}
+    //         />
+    //       </div>
+    //       <span className="text-danger">{datas.value.error.phone}</span>
+    //     </div>
 
-        <div class="form-group">
-          <div className="row">
-            <label for="password" className="mr-3">Password:</label>
-            <TextField
-              type="password"
-              id="password"
-              name="password"
-              variant="standard"  
+    //     <div class="form-group">
+    //       <div className="row">
+    //         <label for="password" className="mr-3">Password:</label>
+    //         <input
+    //           type="password"
+    //           id="password"
+    //           name="password"
+    //           className="form-control"
            
-              required
-              onChange={(e) => dispatch(changePassword(e.target.value))}
-            />
-          </div>
-        </div>
+    //           required
+    //           onChange={(e) => dispatch(changePassword(e.target.value))}
+    //         />
+    //       </div>
+    //     </div>
 
-        <div class="form-group">
-          <div className="row">
-            <label for="password" className="">Retype Password:</label>
-            <TextField
-              type="password"
-              id="password"
-              name="password"
-              variant="standard"  
+    //     <div class="form-group">
+    //       <div className="row">
+    //         <label for="password" className="">Retype Password:</label>
+    //         <input
+    //           type="password"
+    //           id="password"
+    //           name="password"
+                
+    //           className="form-control"
            
-              required
-              onChange={(e) => dispatch(changeCpassword(e.target.value))}
-            />
-          </div>
-          <span className="text-danger">{datas.value.error.cpassword}</span>
-        </div>
+    //           required
+    //           onChange={(e) => dispatch(changeCpassword(e.target.value))}
+    //         />
+    //       </div>
+    //       <span className="text-danger">{datas.value.error.cpassword}</span>
+    //     </div>
 
-        <p class="mt-3 text-center">
-          Already have an account? <a href="login.html">Login</a>
-        </p>
-      </div>
-    </div>
+    //     <p class="mt-3 text-center">
+    //       Already have an account? <a href="login.html">Login</a>
+    //     </p>
+    //   </div>
+    // </div>
   );
 };
 
